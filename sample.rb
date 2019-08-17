6fea739629d5e7fa07b0ea8847024dc6bbec3ad5
杉山貴哉 <s.takaya1027@gmail.com>
2019-08-17 12:38:43 +0900

    他の文字を防ぐ処理を追加

--- sample.rb
+++ sample.rb
@@ -1,7 +1,15 @@
 class Player
   def hand
     # コンソールを入力待ち状態にし、プレイヤーがコンソールから打ち込んだ値を出力する処理のメソッドの処理をこの中に作成する
-    @hand = gets.to_i
+    @hand = gets.chomp
+    #空白文字を防ぐ
+    unless @hand.empty? 
+      #その他の文字を防ぐ
+      hand = @hand.to_i
+      unless hand == 0 || hand == 1 || hand == 2 
+         @hand = @hand.to_i
+      end
+    end
     @hand
   end
 end
@@ -20,17 +28,17 @@ class Janken
     # その際、あいこもしくはグー、チョキ、パー以外の値入力時には、もう一度ジャンケンをする
     # 相手がグー、チョキ、パーのうち、何を出したのかも表示させる
     options = %w(グー チョキ パー)
-    if  player_hand < 0 || player_hand > 2 
-       puts "0〜2の数字を入力してください。"
-    elsif (player_hand - enemy_hand + 3)  % 3 == 2
+    if player_hand == 2 && enemy_hand == 0 || player_hand == 1 && enemy_hand == 2 || player_hand == 0 && enemy_hand == 1
       puts "相手の手は#{options[enemy_hand]}です。あなたの勝ちです。"
       exit
-    elsif (player_hand - enemy_hand + 3)  % 3 == 1
+    elsif player_hand == 2 && enemy_hand == 1 || player_hand == 1 && enemy_hand == 0 || player_hand == 0 && enemy_hand == 1
       puts "相手の手は#{options[enemy_hand]}です。あなたの負けです。"
       exit
-    else
+    elsif  player_hand == 0 && enemy_hand == 0 || player_hand == 0 && enemy_hand == 0 || player_hand == 1 && enemy_hand == 1
       puts "あいこです。"
-    end
+    else  
+      puts "0〜2の数字を入力してください。"
+   end
   end
 end
 
